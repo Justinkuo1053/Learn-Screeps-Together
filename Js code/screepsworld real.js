d7f181b5e211c98dd60ceaee1ec3bb6e2f096b71
justinkuo1053 <justinkuo719@gmail.com>
2025-10-11 15:24:11 +0800

    Initial import: JS and Go versions of Screeps starter

---
+++ Js code/screepsworld real.js
@@ -0,0 +1,31 @@
+
+
+// 基礎主程式 （持續按照tick回合制）
+
+module.exports.loop = function () {
+
+	const spawn = Game.spawns['Spawn1']
+
+	}
+
+}
+
+// 清理記憶體
+
+// 計算個角色數量
+
+// 顯示角色數量
+
+// 自動生產
+
+type Game struct {
+    Spawns 
+}
+
+type Spawn struct {
+    Name String
+    Energy int
+}
+
+func (s *Spawn) 
+
